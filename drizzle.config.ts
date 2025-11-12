867814eed3aec1d69b98bc3345c241c3d0a5dd96
citron <45784494+lcandy2@users.noreply.github.com>
2025-11-12 12:14:29 +0800

    drizzle deps

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -22,7 +22,7 @@ function getLocalD1DB() {
 
 export default defineConfig({
     dialect: "sqlite",
-    schema: "./src/db/index.ts",
+    schema: "./src/lib/db/index.ts",
     out: "./drizzle",
     ...(process.env.NODE_ENV === "production"
         ? {
