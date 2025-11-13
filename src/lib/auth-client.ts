a4685756418f0508bd5363d85211cf0c04d2b2cd
citron <45784494+lcandy2@users.noreply.github.com>
2025-11-13 12:05:07 +0800

    remove all files

--- src/lib/auth-client.ts
+++
@@ -1,6 +0,0 @@
-import { createAuthClient } from "better-auth/svelte";
-import { cloudflareClient } from "better-auth-cloudflare/client";
-
-export const authClient = createAuthClient({
-  plugins: [cloudflareClient()],
-});
